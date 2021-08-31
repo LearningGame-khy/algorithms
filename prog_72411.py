313d0402492aac0c87a8f5c51b69ec79c4263641
LearningGame-khy <LearningGame.khy@gmail.com>
2021-08-31 17:10:22 +0900

    메뉴 리뉴얼 1차 제출(테스트 통과실패) 테스트 3번에서 XY만 출력됨

---
+++ prog_72411.py
@@ -0,0 +1,49 @@
+### 메뉴 리뉴얼
+# 2 <= orders <= 20  2 <= orders.item <= 10
+# 1 <= course <= 10
+# answer 오름차순, 동일할경우 모두 배열에 담아서
+from itertools import combinations
+
+def solution(orders, course):
+    answer = []
+    temp = []
+
+    orders = sorted(orders)
+    for order in orders:
+        order = list(order)
+        for course_cnt in course:
+            for menu in combinations(order, course_cnt):
+                new_menu = ''.join(menu)
+                temp.append(new_menu)
+
+    check = {}
+    for menu in temp:
+        if menu not in check:
+            check[menu] = 1
+        else:
+            check[menu] += 1
+
+    for course_cnt in course:
+        max = 0
+        temp = []
+        for menu in check.keys():
+            if len(menu) != course_cnt or check[menu] < 2:
+                continue
+
+            if check[menu] > max:
+                temp = []
+                max = check[menu]
+                temp.append(menu)
+
+            elif check[menu] == max:
+                temp.append(menu)
+
+        answer += temp
+
+    return sorted(answer)
+
+if __name__ == "__main__":
+    orders = ["XYZ", "XWY", "WXA"]
+    course = [2,3,4]
+
+    print(solution(orders, course))
